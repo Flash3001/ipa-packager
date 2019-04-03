a8242eb67ee9e7d6534976d4eb968cc491cf93c5
Lucas Teixeira <flash3001@msn.com>
2019-04-02 22:46:00 -0300

    Remove unneeded archs from dylibs

--- package_ipa_default.sh
+++ package_ipa_default.sh
@@ -33,10 +33,35 @@ echo "+ Adding SWIFT support (if necessary)"
 if [ -d "${APP}/Frameworks" ];
 then
     mkdir -p "${TEMP_IPA_BUILT}/SwiftSupport"
+    mkdir -p "${TEMP_IPA_BUILT}/SwiftSupport/iphoneos"
 
-    for SWIFT_LIB in $(ls -1 "${APP}/Frameworks/"); do
+    for SWIFT_LIB in $(ls -1 "${APP}/Frameworks/" | grep libswift); do
         echo "Copying ${SWIFT_LIB}"
-        cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${TEMP_IPA_BUILT}/SwiftSupport"
+        
+        SOURCE="${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}"
+        SWIFT_LIB_FULL_PATH="${APP}/Frameworks/${SWIFT_LIB}"
+
+        ARC_TO_REMOVE=($(lipo "${SOURCE}" -archs))
+        ARC_APP=($(lipo "${SWIFT_LIB_FULL_PATH}" -archs))
+
+        for archApp in "${ARC_APP[@]}"; do 
+            for i in "${!ARC_TO_REMOVE[@]}"; do 
+                if [[ ${ARC_TO_REMOVE[i]} = "${archApp}" ]]; then
+                    unset ARC_TO_REMOVE[i]
+                fi
+            done 
+        done
+
+        ARGS=()
+        for i in "${!ARC_TO_REMOVE[@]}"; do
+            ARGS+=" -remove ${ARC_TO_REMOVE[i]}"
+        done
+
+        if [ ${#ARGS[@]} == 0 ]; then
+            ARGS+=" -create"
+        fi
+
+        lipo "${SOURCE}" ${ARGS} -output "${TEMP_IPA_BUILT}/SwiftSupport/iphoneos/${SWIFT_LIB}"
     done
 fi
 
